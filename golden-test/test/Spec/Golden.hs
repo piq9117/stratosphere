8457f700bdc94448bc66cf2b2a61420a1e3398b8
piq9117 <piq9117@gmail.com>
2025-09-23 17:48:20 -0400

    add autoscaling golden test

--- golden-test/test/Spec/Golden.hs
+++ golden-test/test/Spec/Golden.hs
@@ -1,21 +1,22 @@
 module Spec.Golden (runGoldenTest) where
 
+import qualified Stratosphere
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsStringDiff)
-import qualified Stratosphere 
 import Prelude
 
 runGoldenTest :: FilePath -> Stratosphere.Template -> TestTree
 runGoldenTest outputFilename template = do
-  let testDirectory = "test/stratosphere/"
+  let 
+    testDirectory :: FilePath
+    testDirectory = "test/stratosphere/"
   let encodedTemplate = Stratosphere.encodeTemplate template
 
-  testGroup 
+  testGroup
     "stratosphere encode <=> decode"
-    [ goldenVsStringDiff 
+    [ goldenVsStringDiff
         outputFilename
         (\ref new -> ["diff", "-u", ref, new])
         (testDirectory <> outputFilename)
         (pure encodedTemplate)
     ]
-
